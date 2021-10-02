1bfca5c362f1588a89ad2dac3979cfcc49337cdb
ESinicin <catpuff13337@gmail.com>
2021-10-02 15:52:47 +0300

    Added Test project and tested person and company services.

---
+++ Tests/Services/CompanyServiceTest.cs
@@ -0,0 +1,117 @@
+﻿using BLL.Services;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Services
+{
+    [TestClass]
+    public class CompanyServiceTest
+    {
+        private Mock<IRepository<Company>> _repository;
+        private CompanyService _service;
+
+        private List<Person> _people;
+        private List<Company> _companies;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _repository = new Mock<IRepository<Company>>();
+            _service = new CompanyService(_repository.Object);
+
+            InitMockEntities();
+        }
+
+        [TestMethod]
+        public void GetEmployeeCount_PassCompanyIdWithLessThan10Employees()
+        {
+            var companyId = 1;
+            int expectedCount = _companies.FirstOrDefault(x => x.Id == companyId)
+                .People
+                .Count;
+
+            _repository.Setup(x => x.GetAll()).Returns(_companies);
+            var actualCount = _repository.Object.GetAll()
+                .ToList()
+                .FirstOrDefault(x => x.Id == companyId)
+                .People
+                .Count;
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void GetEmployeeCount_PassCompanyIdWithMoreThan10Employees()
+        {
+            var companyId = 2;
+            int expectedCount = _companies.FirstOrDefault(x => x.Id == companyId)
+                .People
+                .Count;
+
+            _repository.Setup(x => x.GetAll()).Returns(_companies);
+            
+            Assert.ThrowsException<Exception>(() =>
+            {
+                var actualCount = _service.GetEmployeeCount(companyId);
+            });
+        }
+
+        private void InitMockEntities()
+        {
+            _people = new List<Person>()
+            {
+                new Person() { Id = 1, Name = "Матвей Радуга", Age = 36 },
+                new Person() { Id = 2, Name = "Акуна Матата", Age = 14 },
+                new Person() { Id = 3, Name = "Хан Карыдов", Age = 23 },
+                new Person() { Id = 4, Name = "Ярослав Лютобор", Age = 55 },
+                new Person() { Id = 5, Name = "Саммерсет Оушен", Age = 91 },
+                new Person() { Id = 6, Name = "Ыхлас Ырбетов", Age = 16 },
+                new Person() { Id = 7, Name = "Салат Петухов", Age = 17 },
+                new Person() { Id = 8, Name = "Хьюберт Блейн", Age = 26 },
+                new Person() { Id = 9, Name = "Вольф Флегштайн", Age = 31 },
+                new Person() { Id = 10, Name = "Барнаби Мармадюк", Age = 20 },
+                new Person() { Id = 11, Name = "Блюриа Азия", Age = 21 },
+                new Person() { Id = 12, Name = "Элси Выдра", Age = 49 },
+                new Person() { Id = 13, Name = "Чарли Волк", Age = 25 },
+                new Person() { Id = 14, Name = "Зоуи Дешанель", Age = 30 },
+                new Person() { Id = 15, Name = "Сэм Уортингтон", Age = 18 },
+                new Person() { Id = 16, Name = "Джейсона Ли", Age = 17 },
+            };
+
+            _companies = new List<Company>()
+            {
+                new Company
+                {
+                    Id = 1,
+                    Name = "ООО «‎Хахали твоей мамки»",
+                    People = new List<Person>()
+                },
+                new Company
+                {
+                    Id = 2,
+                    Name = "«Пидрильный клуб любителей пощекотать очко»",
+                    People = new List<Person>()
+                },
+            };
+
+            _people.ForEach(x =>
+            {
+                if (x.Id % 3 == 0)
+                {
+                    x.Company = _companies[0];
+                    _companies[0].People.Add(x);
+                }
+                else
+                {
+                    x.Company = _companies[1];
+                    _companies[1].People.Add(x);
+                }
+            });
+        }
+    }
+}
---
+++ Tests/Services/PersonServiceTest.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Services;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using UI.Profiles;
+
+namespace Tests.Services
+{
+    [TestClass]
+    public class PersonServiceTest
+    {
+        private Mock<IRepository<Person>> _repository;
+        private IMapper _mapper;
+        private PersonService _service;
+
+        private List<Person> _people;
+        private List<Company> _companies;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _repository = new Mock<IRepository<Person>>();
+            _mapper = new Mapper(new MapperConfiguration(cfg => 
+            {
+                cfg.AddProfile<PersonProfile>();
+                cfg.AddProfile<CompanyProfile>();
+            }));
+            _service = new PersonService(_mapper, _repository.Object);
+
+            InitMockEntities();
+        }
+        
+        [TestMethod]
+        public void GetPersonsWorksOn_ValidCall()
+        {
+            var companyId = 1;
+            var expected = _mapper.Map<List<Person>, List<PersonDTO>>(_people)
+                .Where(x => x.Company.Id == companyId)
+                .ToList();
+
+            _repository.Setup(x => x.GetAll()).Returns(_people);
+
+            var actual = _service.GetPersonsWorksOn(companyId);
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (var i = 0; i < actual.Count; i++)
+            {
+                Assert.IsNotNull(actual[i]);
+
+                Assert.AreEqual(actual[i].Id, expected[i].Id);
+                Assert.AreEqual(actual[i].Name, expected[i].Name);
+                Assert.AreEqual(actual[i].Age, expected[i].Age);
+
+                Assert.IsNotNull(actual[i].Company);
+
+                Assert.AreEqual(actual[i].Company.Id, expected[i].Company.Id);
+                Assert.AreEqual(actual[i].Company.Name, expected[i].Company.Name);
+            }
+        }
+
+        private void InitMockEntities()
+        {
+            _people = new List<Person>()
+            {
+                new Person() { Id = 1, Name = "Матвей Радуга", Age = 36 },
+                new Person() { Id = 2, Name = "Акуна Матата", Age = 14 },
+                new Person() { Id = 3, Name = "Хан Карыдов", Age = 23 },
+                new Person() { Id = 4, Name = "Ярослав Лютобор", Age = 55 },
+                new Person() { Id = 5, Name = "Саммерсет Оушен", Age = 91 },
+                new Person() { Id = 6, Name = "Ыхлас Ырбетов", Age = 16 },
+                new Person() { Id = 7, Name = "Салат Петухов", Age = 17 },
+                new Person() { Id = 8, Name = "Хьюберт Блейн", Age = 26 },
+                new Person() { Id = 9, Name = "Вольф Флегштайн", Age = 31 },
+                new Person() { Id = 10, Name = "Барнаби Мармадюк", Age = 20 },
+                new Person() { Id = 11, Name = "Блюриа Азия", Age = 21 },
+                new Person() { Id = 12, Name = "Элси Выдра", Age = 49 },
+                new Person() { Id = 13, Name = "Чарли Волк", Age = 25 },
+                new Person() { Id = 14, Name = "Зоуи Дешанель", Age = 30 },
+                new Person() { Id = 15, Name = "Сэм Уортингтон", Age = 18 },
+                new Person() { Id = 16, Name = "Джейсона Ли", Age = 17 },
+            };
+
+            _companies = new List<Company>()
+            {
+                new Company
+                {
+                    Id = 1,
+                    Name = "ООО «‎Хахали твоей мамки»",
+                    People = new List<Person>()
+                },
+                new Company
+                {
+                    Id = 2,
+                    Name = "«Пидрильный клуб любителей пощекотать очко»",
+                    People = new List<Person>()
+                },
+            };
+
+            _people.ForEach(x =>
+            {
+                if (x.Id % 3 == 0)
+                {
+                    x.Company = _companies[0];
+                    _companies[0].People.Add(x);
+                }
+                else
+                {
+                    x.Company = _companies[1];
+                    _companies[1].People.Add(x);
+                }
+            });
+        }
+    }
+}
